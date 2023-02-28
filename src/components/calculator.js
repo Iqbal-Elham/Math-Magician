46a2287976f2e9d9abe6e9bb1d40d664c80b102e
iqbal-elham <iqbal.elham7@gmail.com>
2023-03-01 01:29:28 +0430

    Update readme file

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import calculate from '../logic/calculate';
-import './Calculator.css';
+import './calculator.css';
 
 const Calculator = () => {
   const [state, setState] = useState({
